a84038d120acecafadf0679ee21ba0e5416d216c
wangchengbing329 <319264106@qq.com>
2019-12-03 10:29:25 +0800

    change

--- my_program/bootstrap-vue/src/main.js
+++ my_program/bootstrap-vue/src/main.js
@@ -5,10 +5,14 @@ import App from './App'
 import router from './router'
 import bootstarpVue from 'bootstrap-vue'
 import vueAwesomeSwiper from 'vue-awesome-swiper'
+import VideoPlayer from 'vue-video-player'
+import "video.js/dist/video-js.css"
+import "vue-video-player/src/custom-theme.css"
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
+Vue.use(VideoPlayer)
 Vue.use(bootstarpVue)
 Vue.use(vueAwesomeSwiper)
 /* eslint-disable no-new */
--- my_program/bootstrap-vue/src/views/index.vue
+++ my_program/bootstrap-vue/src/views/index.vue
@@ -7,8 +7,7 @@
 
 
         <swiper-slide class="slide"><img src="https://images.pexels.com/photos/2973102/pexels-photo-2973102.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></swiper-slide>
-        <swiper-slide class="slide"><img src="https://images.pexels.com/photos/2973102/pexels-photo-2973102.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></swiper-slide>
-        <swiper-slide class="slide"><img src="https://images.pexels.com/photos/2973102/pexels-photo-2973102.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></swiper-slide>
+
         <!-- <swiper-slide class="slide"><img src="https://images.pexels.com/photos/2973102/pexels-photo-2973102.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt=""></swiper-slide> -->
 
         <div class="swiper-pagination" slot="pagination"></div>
@@ -16,9 +15,25 @@
 
 
       </swiper>
+ <div class="player">
+          <video-player  class="vjs-custom-skin"
+                         ref="videoPlayer"
+                         :options="playerOptions"
+                         :playsinline="true"
+                         @play="onPlayerPlay($event)"
+                         @pause="onPlayerPause($event)"
+                         @ended="onPlayerEnded($event)"
+                         @loadeddata="onPlayerLoadeddata($event)"
+                         @waiting="onPlayerWaiting($event)"
+                         @playing="onPlayerPlaying($event)"
+                         @timeupdate="onPlayerTimeupdate($event)"
+                         @canplay="onPlayerCanplay($event)"
+                         @canplaythrough="onPlayerCanplaythrough($event)"
+                         @ready="playerReadied"
+                         @statechanged="playerStateChanged($event)">
+          </video-player>
 
-
-
+</div>
   </div>
 </template>
 
@@ -27,8 +42,8 @@ export default {
     data () {
       return {
         swiperOption: {
-          loop:true,
-          effect: 'coverflow',
+          // loop:true,
+          // effect: 'coverflow',
           grabCursor: true,
           centeredSlides: true,
 
@@ -46,7 +61,66 @@ export default {
           }
 
 
+        },
+         playerOptions: {
+          height: '360',
+          autoplay: true,
+          muted: true,
+          language: 'en',
+          playbackRates: [0.7, 1.0, 1.5, 2.0],
+          sources: [{
+            type: "video/mp4",
+            // mp4
+            src: "https://player.vimeo.com/external/371905764.sd.mp4?s=90602b652998477512f08f9d21702e94d23ad79a&profile_id=139&oauth2_token_id=57447761",
+            // webm
+            // src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+          }],
+          poster: "https://surmon-china.github.io/vue-quill-editor/static/images/surmon-1.jpg",
         }
+      }
+    },  computed: {
+      player() {
+        return this.$refs.videoPlayer.player
+      }
+    }, methods: {
+      // listen event
+      onPlayerPlay(player) {
+        // console.log('player play!', player)
+      },
+      onPlayerPause(player) {
+        // console.log('player pause!', player)
+      },
+      onPlayerEnded(player) {
+        // console.log('player ended!', player)
+      },
+      onPlayerLoadeddata(player) {
+        // console.log('player Loadeddata!', player)
+      },
+      onPlayerWaiting(player) {
+        // console.log('player Waiting!', player)
+      },
+      onPlayerPlaying(player) {
+        // console.log('player Playing!', player)
+      },
+      onPlayerTimeupdate(player) {
+        // console.log('player Timeupdate!', player.currentTime())
+      },
+      onPlayerCanplay(player) {
+        // console.log('player Canplay!', player)
+      },
+      onPlayerCanplaythrough(player) {
+        // console.log('player Canplaythrough!', player)
+      },
+      // or listen state event
+      playerStateChanged(playerCurrentState) {
+        // console.log('player current update state', playerCurrentState)
+      },
+      // player is ready
+      playerReadied(player) {
+        // seek to 10s
+        console.log('example player 1 readied', player)
+        player.currentTime(10)
+        // console.log('example 01: the player is readied', player)
       }
     }
 }
@@ -66,6 +140,6 @@ img{
   display: flex;
 }
 .swiper-container{
-  
+
 }
 </style>
